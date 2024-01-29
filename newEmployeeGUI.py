27bbce401a8cf59f2cf8e4c3c174f02a8ca49066
Armani Sosa <armanibep@gmail.com>
2024-01-29 17:07:07 -0500

    Add method to create new Employee GUI

--- newEmployeeGUI.py
+++ newEmployeeGUI.py
@@ -1,1 +1,12 @@
 from modules import *
+
+
+
+
+#Create New EmployeeGUI
+def newEmployeeWdw(root,guiInstance):
+    #Window specs
+    new_window = tb.Toplevel(root)
+    new_window.title("Employee Information")
+    new_window.geometry("600x500")
+    new_window.resizable(False, False)
