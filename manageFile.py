6d51c6a149221de6e6a481a56ce584c7c474fb03
Armani Sosa <armanibep@gmail.com>
2024-01-29 16:41:51 -0500

    Check if file exist, return file

--- manageFile.py
+++ manageFile.py
@@ -5,3 +5,12 @@ from modules import json
 def getEmployeeList():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, 'employee_data.json')
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            try:
+                data = json.load(file)
+                return data
+            except json.JSONDecodeError as e:
+                return None
+    else:
+        return None
