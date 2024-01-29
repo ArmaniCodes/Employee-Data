dc435cde0d3ee7a280788ca809a93a783d967c56
Armani Sosa <armanibep@gmail.com>
2024-01-29 18:03:28 -0500

    Setup window

--- editEmployeeGUI.py
+++ editEmployeeGUI.py
@@ -6,3 +6,9 @@ from modules import *
 def employeeInfoWdw(root,name,guiInstance):
     #Get the button that stores this individual employee's name
     buttonInstance = guiInstance.buttonList[name]
+
+    #Window specs
+    new_window = tb.Toplevel(root)
+    new_window.title(name + "'s info")
+    new_window.geometry("600x500")
+    new_window.resizable(False, False)
