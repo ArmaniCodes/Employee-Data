ef8d423515ec862acdbde50be1c315be98d9f7da
Armani Sosa <armanibep@gmail.com>
2024-01-29 18:45:01 -0500

    Check if employee exist, if so display info

--- sortEmployees.py
+++ sortEmployees.py
@@ -2,4 +2,7 @@ from editEmployeeGUI import employeeInfoWdw
 from manageFile import getEmployeeList
 
 def searchForEmployee(root,guiInstance,name):
-    pass
+    empList = getEmployeeList()
+    #If emply exist then we can call employeeInfoWdw which will load the data visually and allow us to edit it
+    if name in empList:
+        employeeInfoWdw(root,name,guiInstance)
