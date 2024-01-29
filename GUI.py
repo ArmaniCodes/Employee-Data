53d21092a8e2c51278438bd53975e8f70d792508
Armani Sosa <armanibep@gmail.com>
2024-01-29 16:02:22 -0500

    Setup window properties

--- GUI.py
+++ GUI.py
@@ -3,4 +3,8 @@ from modules import *
 
 class EmployeeGui:
     def __init__(self):
+        #Dark Theme
         self.root = tb.Window(themename="darkly")
+        #Window Size
+        self.root.geometry("1150x500")
+        self.root.resizable(False, False)
