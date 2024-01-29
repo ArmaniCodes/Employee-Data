dbe1b2932a8958ab7cd38a5cf6c8dd85770460e4
Armani Sosa <armanibep@gmail.com>
2024-01-29 17:08:44 -0500

    Add Title and save button

--- newEmployeeGUI.py
+++ newEmployeeGUI.py
@@ -10,3 +10,11 @@ def newEmployeeWdw(root,guiInstance):
     new_window.title("Employee Information")
     new_window.geometry("600x500")
     new_window.resizable(False, False)
+
+    #Title
+    label = tb.Label(new_window, text="Enter your employees info", font=("Helvetica", 13)).pack(pady=5)
+
+    #Save Button
+    close_button = tb.Button(new_window, text="Save", command=lambda: save(new_window, entryList, guiInstance),
+                             bootstyle="success,outline")
+    close_button.pack(side="bottom")
