0c82adf758d1fc2e15afaf56ea6e652a7417f499
Armani Sosa <armanibep@gmail.com>
2024-01-29 19:21:48 -0500

    Setup statistic models

--- employeeStats.py
+++ employeeStats.py
@@ -1,3 +1,6 @@
 class EmployeeStatistics():
     def __init__(self):
-        pass
+        self.employeeCount = 0
+        self.maleCount = 0
+        self.femaleCount = 0
+        self.employmentType = {"Full Time": 0, "Part Time": 0, "Internship": 0, "Contract": 0}
