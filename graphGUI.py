9bcdf5e9b2c9cf12eda6750b20bc4d310436e9bf
Armani Sosa <armanibep@gmail.com>
2024-01-29 19:16:33 -0500

    Setup graphGUI class

--- graphGUI.py
+++ graphGUI.py
@@ -1,3 +1,8 @@
 from Modules import *
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
+class graphGUI():
+    def __init__(self,root):
+        pass
