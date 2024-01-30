3cd3e1fa6f923edc287a2ed71ffda4df6179f716
Armani Sosa <armanibep@gmail.com>
2024-01-29 19:22:11 -0500

    Add methods to handle incrementing/decrementing stats

--- employeeStats.py
+++ employeeStats.py
@@ -4,3 +4,13 @@ class EmployeeStatistics():
         self.maleCount = 0
         self.femaleCount = 0
         self.employmentType = {"Full Time": 0, "Part Time": 0, "Internship": 0, "Contract": 0}
+
+
+    def increaseMaleCount(self):
+        self.maleCount+=1
+    def increaseFemaleCount(self):
+        self.femaleCount+=1
+    def decreaseMaleCount(self):
+        self.maleCount-=1
+    def decreaseFemaleCount(self):
+        self.femaleCount-=1
