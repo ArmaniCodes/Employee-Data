f71223789709811d3928c4394549412a942c752a
Armani Sosa <armanibep@gmail.com>
2024-01-29 19:17:38 -0500

    Create method to create graphFrame

--- graphGUI.py
+++ graphGUI.py
@@ -6,3 +6,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 class graphGUI():
     def __init__(self,root):
         self.root = root
+        self.createGraphFrame(self.root)
+
+    def createGraphFrame(self, root):
+        self.graphFrame = tb.Frame(root, style="My.TFrame", width=750, height=430)
+        self.graphFrame.place(x=380, y=20)
