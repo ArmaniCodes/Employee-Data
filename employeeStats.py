12cd4255a2394758fcd24a616ebf4cf6671c65c9
Armani Sosa <armanibep@gmail.com>
2024-01-31 16:58:30 -0500

    Retrieve info and return as dictionary

--- employeeStats.py
+++ employeeStats.py
@@ -23,3 +23,12 @@ def get_employment_type_counts():
     #Connect to db
     conn = sqlite3.connect('employee_data.db')
     cur = conn.cursor()
+
+    cur.execute('''
+                   SELECT status, COUNT(*) as count
+                   FROM employees
+                   WHERE status IN ('Full Time', 'Part Time', 'Internship', 'Contract')
+                   GROUP BY status
+               ''')
+    employment_type_counts = dict(cur.fetchall())
+    return employment_type_counts
