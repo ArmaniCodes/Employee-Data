317161e138670473b488b34623fb90267bef66ee
Armani Sosa <armanibep@gmail.com>
2024-01-31 17:13:53 -0500

    Adjust typos

--- sortEmployees.py
+++ sortEmployees.py
@@ -6,16 +6,16 @@ def searchForEmployee(root,guiInstance,name):
         employeeInfoWdw(root, name, guiInstance)
 
 def sortByName(LoadEmployeesMethod):
-    data = get_all_employees_sorted_sql("salary")
-    if data:
-        LoadEmployeesMethod(data)
-
-def sortByID(LoadEmployeesMethod):
     data = get_all_employees_sorted_sql("name")
     if data:
         LoadEmployeesMethod(data)
 
-def sortBySalary(LoadEmployeesFunction):
+def sortByID(LoadEmployeesMethod):
     data = get_all_employees_sorted_sql("employee_id")
+    if data:
+        LoadEmployeesMethod(data)
+
+def sortBySalary(LoadEmployeesFunction):
+    data = get_all_employees_sorted_sql("salary")
     if data:
         LoadEmployeesMethod(data)
