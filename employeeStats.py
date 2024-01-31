0717492f76f70b57d655fff954bb784d15ae3960
Armani Sosa <armanibep@gmail.com>
2024-01-31 16:56:34 -0500

    Connect and retrieve gender/sex as a dictionary

--- employeeStats.py
+++ employeeStats.py
@@ -2,4 +2,15 @@ import sqlite3
 
 
 def get_sex_counts():
-    pass
+    # Connect to db
+    conn = sqlite3.connect('employee_data.db')
+    cur = conn.cursor()
+
+    cur.execute('''
+           SELECT gender, COUNT(*) as count
+           FROM employees
+           GROUP BY gender
+       ''')
+
+    #Retrieve as dictionary
+    gender_counts = dict(cur.fetchall())
