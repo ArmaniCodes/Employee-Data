82de12139e01c165f1f70b00c1d12c363034229c
Armani Sosa <armanibep@gmail.com>
2024-01-31 15:56:15 -0500

    Add method to validate salary input

--- inputValidation.py
+++ inputValidation.py
@@ -4,12 +4,18 @@ from datetime import datetime
 
 
 def checkBirthDate(data):
+    #We try to retrieve the DOB from the user input
     try:
         birthdate = datetime.strptime((data["Date Of Birth"].get()), '%m/%d/%Y')
     except:
+        #We warn the user since the DOB supplied is invalid
         dobentry = data["Date Of Birth"]
         dobentry.delete(0, tb.END)
         dobentry.insert(0, 'mm/dd/yyyy')
         dobentry.config(foreground='red')
         return False
+
+    #Else return True
     return True
+
+def checkSalary(data):
