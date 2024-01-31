d8b6096fe381b23680f4b616fc3008d67c5decfb
Armani Sosa <armanibep@gmail.com>
2024-01-31 15:57:23 -0500

    Input validate salary

--- inputValidation.py
+++ inputValidation.py
@@ -19,3 +19,17 @@ def checkBirthDate(data):
     return True
 
 def checkSalary(data):
+    #Get button instance and button text
+    button = data["Salary"]
+    salary = data["Salary"].get()
+
+    #Input validate salary
+    if salary is None or salary == '' or not (salary.isnumeric()):
+        #Warn user since input is invalid and return False
+        button.delete(0,tb.END)
+        button.insert(0,'integer ONLY!')
+        button.config(foreground='red')
+        return False
+
+    #else return True
+    return True
