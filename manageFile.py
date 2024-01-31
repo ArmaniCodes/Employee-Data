ca8917ddf060cf4823186575076163d4dfef8729
Armani Sosa <armanibep@gmail.com>
2024-01-31 15:29:50 -0500

    Sets up schema for db

--- manageFile.py
+++ manageFile.py
@@ -5,3 +5,28 @@ def addEmployee(data,name):
     #Connection to local db
     conn = sqlite3.connect('employee_data.db')
     cur = conn.cursor()
+
+    # Creates Table if not already existing
+    cur.execute('''
+           CREATE TABLE IF NOT EXISTS employees (
+               id INTEGER PRIMARY KEY,
+               name TEXT,
+               date_of_birth TEXT,
+               gender TEXT,
+               race TEXT,
+               email TEXT,
+               phone TEXT,
+               address TEXT,
+               employee_id TEXT,
+               job_title TEXT,
+               department TEXT,
+               manager_leader TEXT,
+               status TEXT,
+               salary INTEGER,
+               time_and_attendance TEXT,
+               certifications_skills TEXT,
+               training_programs TEXT,
+               extra_information TEXT,
+               age INTEGER
+           )
+       ''')
